87a86e2cd9108cd0e1ea45e09708707da416e2b7
raghavgupta2412 <raghav.gupta_cs21@gla.ac.in>
2024-11-13 23:21:09 +0530

    threads

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,11 @@
-export default async function Page() {
+import { UserButton } from "@clerk/nextjs";
+
+export default function Page() {
   return (
     <main>
-      <h1 className="head-text">OnBoarding</h1>
+      <h1 className="head-text">
+        <UserButton />
+      </h1>
     </main>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,47 @@
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
+
+// const isPublicRoute = createRouteMatcher(["/", "/api/webhook/clerk"]);
+// const ignoredRoutes = ["/api/webhook/clerk"];
+
+// export default clerkMiddleware(async (auth, request) => {
+//   if (!isPublicRoute(request)) {
+//     await auth.protect();
+//   }
+//   if (
+//     ignoredRoutes.some((route) => request.nextUrl.pathname.startsWith(route))
+//   ) {
+//     return NextResponse.next();
+//   }
+// });
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+// Define public and ignored routes
 const isPublicRoute = createRouteMatcher(["/", "/api/webhook/clerk"]);
-const ignoredRoutes = ["/api/webhook/clerk"];
 
 export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect();
-  }
-  if (
-    ignoredRoutes.some((route) => request.nextUrl.pathname.startsWith(route))
-  ) {
+  if (isPublicRoute(request)) {
     return NextResponse.next();
   }
+
+  // Protect other routes with Clerk auth
+  await auth.protect();
+  return NextResponse.next();
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
